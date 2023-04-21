60f31a719f2525d0e5fa1cf892bc19ce4c650097
Mark van den Bosch <markvandenbosch@gmail.com>
2023-04-22 00:29:09 +0200

    Numbers and start math

--- lambda.py
+++ lambda.py
@@ -1,4 +1,4 @@
-# Python universe with only single argumented functions
+# Python universe with only single argumented functions - lambda
 #
 # It's illegal to have numbers, strings, operators, control flow etc.
 # PyCon 2019 Tutorial https://www.youtube.com/watch?v=5C6sv7-eTKg
@@ -15,6 +15,9 @@ def RIGHT(a):
         return b # return second argument
     return f
 
+# Passing a 'second' argument is called Currying in lambda, although the function
+# only accepts one argument
+# https://en.wikipedia.org/wiki/Currying
 assert LEFT('v5')('gnd') == 'v5'
 assert RIGHT('v5')('gnd') == 'gnd'
 
@@ -61,3 +64,43 @@ def OR(x):
 assert OR(TRUE)(FALSE) is TRUE
 assert OR(FALSE)(FALSE) is FALSE
 assert OR(FALSE)(TRUE) is TRUE
+
+# Numbers
+# They are not numbers, but behaviours
+# https://en.wikipedia.org/wiki/Church_encoding
+# API for a number f(x)
+
+ZERO = lambda f: lambda x: x   # no use of the 'f' function, same impl as false. Do nothing
+ONE = lambda f: lambda x: f(x) # do one function call on x
+TWO = lambda f: lambda x: f(f(x)) # Two functions call on x
+THREE = lambda f: lambda x: f(f(f(x)))
+FOUR = lambda f: lambda x: f(f(f(f(x))))
+
+# Debug, so that you can 'see' what the behaviour is of the functions
+def incr(x):
+    return x + 1    # Illegal in rules
+
+assert incr(0) == 1
+assert incr(incr(0)) == 2
+assert THREE(incr)(0) == 3 # do three times incr with starting point 0
+# doing FOUR(THREE) which will be 81... crazy! :0
+assert FOUR(THREE)(incr)(0) == 81 # 3^4 -> 3 * 3 * 3 * 3
+assert ZERO(incr)(0) == 0
+
+# Math
+# Successor, which will get to the next number
+# SUCC(TWO) --> THREE
+# The 'API' of a number is f(x), in order to implement a successor
+# you must add an other function too it and then call the API of number.
+SUCC = lambda n: (lambda f: lambda x: f(n(f)(x)))
+
+assert SUCC(FOUR)(incr)(0) == 5 # successor of 4 -> 5
+assert SUCC(SUCC(FOUR))(incr)(0) == 6
+
+# Add
+ADD = lambda x: lambda y: y(SUCC)(x) # Take SUCC and apply it times the other
+assert ADD(FOUR)(THREE)(incr)(0) == 7
+
+# Multi
+MUL = lambda x: lambda y: lambda f: y(x(f)) # Execute y times the function with x
+assert MUL(FOUR)(THREE)(incr)(0) == 12
